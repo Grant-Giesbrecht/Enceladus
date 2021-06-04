1f61496ad2f1085cedc555cf2aeda0d05826ace7
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-04 13:46:03 -0600

    mapContains: Added depecation warning. Function is superfluous

--- functions/mapContains.m
+++ functions/mapContains.m
@@ -1,5 +1,8 @@
 function tf=mapContains(M, k)
+% TODO: Replace this function with M.isKey(k). isKey is built-in and does
+% the exact same thing!
 
 	tf = ~isempty(find(contains(M.keys, k)));
+	warning("MSTD DEPRECATION NOTICE: Replace the function mapContains() with <map>.isKey(<key>)!");
 
 end
