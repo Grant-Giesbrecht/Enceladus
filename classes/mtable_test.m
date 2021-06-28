3b12169f75559307e767afae1e541306f3e9a322
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-27 20:01:04 -0600

    mtable_test: Created demo

---
+++ classes/mtable_test.m
@@ -0,0 +1,15 @@
+mt = MTable();
+
+mt.row(["", "Chan. 1", "Ch2", "Vdd", "Date"]);
+mt.row(["T1", "5", "3", "12", "12-6-2021"]);
+mt.row(["T2", "6", "3.5", "12", "12-6-2021"]);
+mt.row(["T3", "7", "200.8", "11", "12-6-2021"]);
+mt.table_title("Demo Data");
+
+displ(mt.str());
+
+mt.alignac('r');
+mt.alignt('c');
+mt.alignah('r');
+
+displ(newline, mt.str());
