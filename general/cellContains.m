461776c20fbd920ca15f615bed77e7b4eb07e7c5
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-07-09 12:53:22 -0600

    Create cellContains.m

---
+++ general/cellContains.m
@@ -0,0 +1,47 @@
+function tf = cellContains(cells_in, value, matchCharStr)
+	
+	% Check for optional argument, convertChar
+	if ~exist('matchCharStr', 'var')
+		matchCharStr = false;
+	end
+	
+	% If match strings and chars, convert input to string if is a char
+	if matchCharStr && isa(value, 'char')
+		value = string(value);
+	end
+
+	tf = false;
+	[vr, vc] = size(value);
+
+	% For each item
+	for cellval = cells_in
+		
+		% Get value
+		val = cellval{1};
+		
+		% If match chars and strings and value is a char, convert to
+		% strings
+		if matchCharStr && isa(val, 'char')
+			val = string(val);
+		end
+		
+		% Check, if arrays, that size matches
+		[er, ec] = size(val);
+		if er ~= vr || ec ~= vc
+			continue;
+		end
+		
+		% Not a match if different types
+		if class(val) ~= class(value)
+			continue;
+		end
+
+		% Check if values match
+		if val == value
+			tf = true;
+			return;
+		end
+		
+	end
+	
+end
