846cb6a93fc00b11bb2396ce259b73e61d961c1e
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-07-19 16:11:11 -0600

    MDF_Test: Added smith chart plotting

--- Tutorials/MDF_Test.m
+++ Tutorials/MDF_Test.m
@@ -1,5 +1,7 @@
-mdf = AWRLPmdf;
+%% Read File
+%
 
+mdf = AWRLPmdf;
 
 % mdf.debug = true;
 if ~mdf.load("LP_Data_L1_Dev.mdf")
@@ -7,11 +9,32 @@ if ~mdf.load("LP_Data_L1_Dev.mdf")
 	return;
 end
 
+%% Show some data
+%
+
 displ(mdf.str());
 
 mdf.showBlock(1);
 
 b1 = mdf.bdata{1};
 
+%% Plot Harmonics
+%
 
+a2s = [];
+b2s = [];
+for idx = 1:length(mdf.bdata)
+
+	aidx = mdf.bdataIndex("a2(3)");
+	bidx = mdf.bdataIndex("b2(3)");
+	
+	avar = mdf.bdata{idx}(aidx);
+	bvar = mdf.bdata{idx}(bidx);
+	
+	a2s = addTo(a2s, avar.data(1, 1) + avar.data(1, 2)*sqrt(-1));
+	b2s = addTo(b2s, bvar.data(1, 1) + bvar.data(1, 2)*sqrt(-1));
+	
+end
+
+smithplot(ab2gamma(a2s, b2s), 'LineStyle', 'none', 'Marker', '+');
 
---
+++ electronics/ab2gamma.m
@@ -0,0 +1,5 @@
+function G = ab2gamma(a2, b2)
+
+	G = a2./b2;
+
+end
