58bfd5966fb002f6a76bb805b3616d9fa3e1aba9
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-07-19 11:17:09 -0600

    MDF_Test: Added debug

--- Tutorials/MDF_Test.m
+++ Tutorials/MDF_Test.m
@@ -1,5 +1,10 @@
 mdf = AWRLPmdf;
 
-mdf.load("LP_Data_L1_Dev.mdf");
 
-displ(mdf.str());
+mdf.debug = true;
+if ~mdf.load("LP_Data_L1_Dev.mdf")
+	displ("ERROR: ", mdf.msg)
+else
+	displ(mdf.str());
+end
+
