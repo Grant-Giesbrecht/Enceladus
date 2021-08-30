8b3e724d87b907c1d964c7bc8fa47def2ff98208
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-08-30 08:26:17 -0600

    lin2dB, dB2lin: Added option for log-10 or log-20

--- general/dB2lin.m
+++ general/dB2lin.m
@@ -1,10 +1,18 @@
-function l = dB2lin(x)
+function l = dB2lin(x, mult)
 % DB2LIN Takes a linear value and converts it to decibels.
 %
 %	DB2LIN(X) Calculates the linear value for the decibel value X.
 %
+%	DB2LIN(X, MULT) Calculates the linear value for the decibel value X,
+%	and MULT sets the type of log calculated (log 20 vs. log 10). MULT is
+%	expected to be either the numeric value 10 or 20.
+%
 %	See also LIN2DB
 
-	l = 10.^(x./20);
+	if ~exist('mult', 'var')
+		mult = 20;
+	end
+
+	l = 10.^(x./mult);
 
 end
--- general/lin2dB.m
+++ general/lin2dB.m
@@ -1,10 +1,18 @@
-function db = lin2dB(x)
+function db = lin2dB(x, mult)
 % LIN2DB Takes a linear value and converts it to decibels.
 %
 %	LIN2DB(X) Calculates X in dB
 %
+%	LIN2DB(X, MULT) Calculates the decibel value of the linear value X.
+%	MULT sets the type of log calculated (log 20 vs. log 10). MULT is
+%	expected to be either the numeric value 10 or 20.
+%
 %	See also DB2LIN
 
-	db = 20.*log10(x);
+	if ~exist('mult', 'var')
+		mult = 20;
+	end
+
+	db = mult.*log10(x);
 
 end
