d7f4385d8c098278898cea8f56f67492d50c0e06
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-08-31 10:40:23 -0600

    LPSweep_Demo: Fixed check if need reload MDF

--- Tutorials/LPSweep_Demo.m
+++ Tutorials/LPSweep_Demo.m
@@ -1,7 +1,7 @@
 filename = "LP_3053_4x100_DS3.mdf";
 
 % Create MDF object
-if ~exist('mdf', 'var') || mdf.filename ~= filename
+if ~exist('mdf', 'var') || ~isa(mdf.filename, class(filename)) || mdf.filename ~= filename
 
 	mdf = AWRLPmdf;
 	mdf.debug = false;
