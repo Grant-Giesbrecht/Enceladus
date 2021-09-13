eb4e6392709fe8a069502f735f7742ac56eb23d9
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-09-13 15:21:24 -0600

    Update install.m

--- install.m
+++ install.m
@@ -1,5 +1,5 @@
 % List of relative directories (relative to install.m's location)
-folders = ["general", "plotting", "system", "CLI", "string_manipulation"];
+folders = ["general", "plotting", "system", "CLI", "string_manipulation", "search"];
 
 % Find out where 'install.m' is saved
 file_path = mfilename('fullpath'); % Get full path to this file
