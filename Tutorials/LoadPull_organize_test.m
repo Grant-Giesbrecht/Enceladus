06cc42ab1440632acf7cf3a8781cb47a93812fd8
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-09-15 12:28:49 -0600

    Updated LoadPull_organize_test.m

--- Tutorials/LoadPull_organize_test.m
+++ Tutorials/LoadPull_organize_test.m
@@ -2,5 +2,8 @@ lp = LoadPull;
 
 lp.freq = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2];
 lp.props.iPower = [1,1,2,2,3,3,4,4,5,5,];
+lp.props.iPower = fliplr(lp.props.iPower);
 
 lp.organize("freq", "props.iPower")
+
+lp.showorg();
