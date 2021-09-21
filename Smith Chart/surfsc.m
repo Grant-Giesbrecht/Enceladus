8acf36481d95bfb0d18e4217a75b7f9c9aadbdcc
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-09-21 15:55:11 -0600

    surfsc: Added commands to draw smith chart

--- Smith Chart/surfsc.m
+++ Smith Chart/surfsc.m
@@ -1,5 +1,39 @@
 function surfsc(gamma, val, varargin)
 
+	expectedDomain = {'Z', 'G'};
+	expectedSchemes = {'Light', 'Dark'};
+
+    p = inputParser;
+    p.KeepUnmatched = true;
+	p.addParameter('Domain', "G", @(x) any(validatestring(char(x),expectedDomain)) );
+	p.addParameter('Ax', gca, @(x) isa(x, 'matlab.graphics.axis.Axes') || isa(x, 'matlab.graphics.GraphicsPlaceholder'));
+    p.addParameter('Z0', 50, @isnumeric);
+% 	p.addParameter('ColorVar', [], @isnumeric);
+% 	p.addParameter('Colormap', [], @isnumeric);
+% 	p.addParameter('Scatter', false, @islogical);
+	p.addParameter('Scheme', 'Light', @(x) any(validatestring(char(x), expectedSchemes)) );
+% 	p.addParameter('MSizes', [], @(x) true );
+
+    p.parse(varargin{:});
+	
+	ax = p.Results.Ax;
+	
+	% Get plot arguments
+    tmp = [fieldnames(p.Unmatched),struct2cell(p.Unmatched)];
+    plotArgs = reshape(tmp',[],1)'; 
+	
+	% Get input data as a reflection coefficient
+	if p.Results.Domain == "Z"
+		G = Z2G(gamma, p.Results.Z0);
+	else
+		G = gamma;
+	end
+	
+	% If hold is not applied, redraw smith chart
+	if ~ishold
+		nd = drawsc(ax, p.Results.Scheme);
+	end
+
 	num_real = 100;
 	num_imag = 100;
 
@@ -11,6 +45,8 @@ function surfsc(gamma, val, varargin)
 	% Interpolate the data in 'val' over the new grid
 	V = griddata(re_gamma, im_gamma, val, R, I);
 
-	surf(R, I, V./max(V));
+	surf(R, I, V);
 
+	daspect([1, 1, max(val)]);
+	
 end
