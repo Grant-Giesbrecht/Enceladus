9ba67fad62e9c2ff71948ed04f41f66e494c7b44
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-10-29 07:55:38 -0600

    Changed name of LPContour to ContourSpec

--- Tutorials/vennsc_demo.m
+++ Tutorials/vennsc_demo.m
@@ -3,8 +3,8 @@ lp = file2loadpull("3053_Sweep_EF_Local_h2.mdf");
 lp = lp.get(lp.filter("props.iPower", 10, "freq", 10e9));
 
 % Create contour specifier objects
-c1 = LPContour(lp.gamma(), lp.pae(), "PAE (%)", 40); % PAE >= 40%
-c2 = LPContour(lp.gamma(), lp.p_load(), "Pout (W)", 1); %Pout >= 1W
+c1 = ContourSpec(lp.gamma(), lp.pae(), "PAE (%)", 40); % PAE >= 40%
+c2 = ContourSpec(lp.gamma(), lp.p_load(), "Pout (W)", 1); %Pout >= 1W
 
 
 vennsc([c1, c2]);
