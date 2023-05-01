0f737a033d2ab44559c11044af5245505a169085
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2023-05-01 13:17:46 -0600

    added deprecation notice

--- RF/Stub Match/tlind.m
+++ RF/Stub Match/tlind.m
@@ -2,5 +2,7 @@ function abcd = tlind(Z0, thetad)
 % TLIN Create the ABCD matrix for a transmission line of length theta
 % radians.
 
+	warning("This function is deprecated in favor of microsim's tlin()");
+
 	abcd = tlin(Z0, thetad*pi/180);
 end
